fc9935e787a133b595c8b901f32775355ca92413
hnbnguyen <baohanmimi@gmail.com>
2022-09-19 16:28:04 -0400

    replace hardcoded lists

---
+++ src/Home.js
@@ -0,0 +1,114 @@
+import RecentSong from "./RecentSong";
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+    logo: {
+        fontSize: 50,
+    },
+    content: {
+        marginLeft: 50,
+        marginRight: 50
+    },
+    bio: {
+        display: "flex"
+    },
+    text: {
+        
+    }
+
+})
+
+const ICONS = [
+    {
+        link: "https://www.linkedin.com/in/han-mimi-nguyen/",
+        icon: "https://imgs.search.brave.com/_h2cUFzpOVe2SBVybuOuXDHgANKiUr9zHbfZRqhTFBs/rs:fit:980:980:1/g:ce/aHR0cHM6Ly9jZG4u/b25saW5ld2ViZm9u/dHMuY29tL3N2Zy9p/bWdfNDQ3MjMucG5n",
+        desc: "linkedin profile"
+    },
+
+    {
+        link: "https://drive.google.com/file/d/1C8bJCowHWujhoMY4bQ1osl7I6dIbODBO/view?usp=sharing",
+        icon: "https://imgs.search.brave.com/XS1PQDToIq7-koaLvS4HMci3SEhL_a5HtgER77P5OnM/rs:fit:512:512:1/g:ce/aHR0cHM6Ly9jZG4w/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvZGlnaXRhbC1t/YXJrZXRpbmctMi0x/My81MC8xMjItNTEy/LnBuZw",
+        desc: "resume"
+    },
+
+    {
+        link: "https://github.com/hnbnguyen",
+        icon: "https://imgs.search.brave.com/xzKOD-4sj1kj_paDDdS1eZmHyBACOXggT4F73QwEGnM/rs:fit:981:956:1/g:ce/aHR0cHM6Ly9jZG4u/b25saW5ld2ViZm9u/dHMuY29tL3N2Zy9p/bWdfNDQ2MDUucG5n",
+        desc: "github profile"
+    },
+
+    {
+        link: "https://www.instagram.com/vision5th/",
+        icon: "https://imgs.search.brave.com/-sgLl8E6hkay7lRc4fG5XM_T5teuGhkgh3PHsaY6Qvo/rs:fit:475:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC51/MU5zMmRlTnJZZVct/eFdYLWpKQzVBSGFI/WiZwaWQ9QXBp",
+        desc: "film photography account"
+    }
+];
+
+const CURRENTLY_WORKING_ON = [
+    'Building "Final Minutes", a social media app that can only be accessed with less than 10% battery',
+    'Reading Jonathan Haidt and Scott Young on Antifragility and Ultralearning',
+    'Finding new music & podcasts',
+];
+
+const PEOPLE_WHO_INSPIRE_ME = [
+    'Dries Depoorter - Making art installations using technology as the medium',
+    'Tristan Harris - Founder of Center for Humane Technology',
+    'Tim Coughlan - My highschool history teacher',
+];
+
+const Home = () => {
+    const classes = useStyles();
+
+    return ( 
+        <div className={classes.content}>
+            
+            <h1 className={classes.logo}>mimi nguyen</h1>
+
+            <h2>
+                product &amp; software engineer | QueensU CS | prev @AWS, The Perk Lab
+            </h2>
+
+            <div>
+                {ICONS.map(({link, icon, desc}) => (
+                    <a
+                        target = "_blank"
+                        rel = "noreferrer"
+                        href = {link}
+                    >
+                        <img
+                            src={icon}
+                            width = "30"
+                            height = "30"
+                            alt= {desc}
+                        />
+                    </a>
+                ))}
+            </div>
+            
+            <div className={classes.bio}>
+                <div>
+                    <h2>what i've been up to these days</h2>
+                    <ul>
+                        {CURRENTLY_WORKING_ON.map((item) => <li>{item}</li>)}
+                    </ul>
+                </div>
+
+                <div>
+                    <h2>people that inspire me</h2>
+                    <ul>
+                        {PEOPLE_WHO_INSPIRE_ME.map((item) => <li>{item}</li>)}
+                    </ul>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <RecentSong />
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/RecentSong.js
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+    subheading: {
+        marginBottom: 3,
+    },
+
+    recentSong: {
+        display: 'flex'
+    },
+
+    artist: {
+        fontSize: 20,
+        margin: '10px 20px',
+        '@media (max-width: 768px)': {
+            fontSize: 24,
+        },
+    },
+
+    title: {
+        fontSize: 15,
+        margin: '10px 20px',
+        maxWidth: '20vw',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+        '@media (max-width: 768px)': {
+            maxWidth: '50vw',
+        },
+    },
+
+    recentSongLink: {
+        textDecoration: 'none',
+        color: 'gray',
+        '&:hover': {
+            color: 'black'
+        },
+    },
+
+    albumArt: {
+        width: 120,
+        height: 120,
+    },
+
+    smallTest: {
+        fontSize: 12,
+        marginTop: 3,
+    }
+});
+
+function RecentSong() {
+    const [recentSong, setRecentSong] = useState(null);
+    const classes = useStyles();
+
+    useEffect(() => {
+        axios({
+            method: 'GET',
+            url: 'https://ws.audioscrobbler.com/2.0',
+            params: {
+                method: 'user.getrecenttracks',
+                user: 'nakedguac',
+                api_key: '6c2f4cde5a70abbc7df1b42987aec0de',
+                format: 'json',
+            },
+        }).then((res) => {
+            const recentTrack = res.data.recenttracks.track[0];
+            const newSong = {
+                artist: recentTrack.artist['#text'],
+                title: recentTrack.name,
+                imageSrc:recentTrack.image[2]['#text'],
+                songLink: recentTrack.url,
+            };
+            setRecentSong(newSong);
+        });
+    }, []);
+
+    if (!recentSong) {
+        return null;
+    }
+
+    return (
+        <>
+            <h3 className={classes.subheading}>my most recently spotify activity</h3>
+            <p className={classes.smallTest}>(hopefully it's not embarrassing)</p>
+            <a
+                target="_blank"
+                rel = "noreferrer"
+                className={classes.recentSongLink}
+                href={recentSong.songLink}
+            >
+                <div className={classes.recentSong}>
+                    <img 
+                        className={classes.albumArt}
+                        src={recentSong.imageSrc}
+                        alt="album art"
+                        loading="lazy"
+                    />
+                    <div>
+                        <h3 className={classes.artist}>{recentSong.artist}</h3>
+                        <p className={classes.title}>{recentSong.title}</p>
+                    </div>
+                </div>
+            </a>
+        </>
+    );
+}
+
+export default RecentSong;
